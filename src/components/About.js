89dd44fdc14ee0a5c7de0c15e4f63e72ee07673d
cover-manish <manish@coverfoxmail.com>
2019-10-14 11:59:25 +0530

    react poketimes project

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+    return(
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>sdf sdfsdf sdf sdf sdf sdf sdfsdfsdfsdfsdfsdf sdfdsf</p>
+        </div>
+    )
+}
+
+
+export default About
---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>sdf sdfsdf sdf sdf sdf sdf sdfsdfsdfsdfsdfsdf sdfdsf</p>
+        </div>
+    )
+}
+
+
+export default Contact
